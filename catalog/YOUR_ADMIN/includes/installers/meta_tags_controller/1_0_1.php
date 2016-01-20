e9f3a93692c5acf7999e2202b05cb8beb1c24379
bislewl <bislewl@gmail.com>
2016-01-20 11:50:37 -0600

    add 1.0.1 versioning file

---
+++ catalog/YOUR_ADMIN/includes/installers/meta_tags_controller/1_0_1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  1_0_1.php
+ *
+ * @package
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: bislewl  1/20/2016 11:50 AM Modified in meta_tags_controller
+ */
+
